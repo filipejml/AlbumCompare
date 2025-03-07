acae8a5c54a348e9e207d5af4cfc3ca33041bfb5
filipejml <brincandodedesign@gmail.com>
2025-03-07 09:43:36 -0300

    feat novas funções no compare

--- app/app.py
+++ app/app.py
@@ -69,7 +69,7 @@ def compare():
     album2_id = request.form['album2']
     token = get_spotify_token()
 
-    # Obter informações dos álbuns
+    # Obter informações detalhadas dos álbuns
     album1_info = requests.get(f'https://api.spotify.com/v1/albums/{album1_id}', headers={'Authorization': f'Bearer {token}'}).json()
     album2_info = requests.get(f'https://api.spotify.com/v1/albums/{album2_id}', headers={'Authorization': f'Bearer {token}'}).json()
 
@@ -77,18 +77,37 @@ def compare():
     album1_tracks = get_album_tracks(album1_id, token)
     album2_tracks = get_album_tracks(album2_id, token)
 
-    # Extrair nomes das músicas
-    album1_track_names = {track['name'] for track in album1_tracks['items']}
-    album2_track_names = {track['name'] for track in album2_tracks['items']}
+    # Extrair nomes e preview_url das músicas
+    album1_tracks_info = [{'name': track['name'], 'preview_url': track['preview_url']} for track in album1_tracks['items']]
+    album2_tracks_info = [{'name': track['name'], 'preview_url': track['preview_url']} for track in album2_tracks['items']]
 
     # Comparar músicas
-    common_tracks = list(album1_track_names.intersection(album2_track_names))
-    unique_to_album1 = list(album1_track_names - album2_track_names)
-    unique_to_album2 = list(album2_track_names - album1_track_names)
+    album1_track_names = {track['name'] for track in album1_tracks_info}
+    album2_track_names = {track['name'] for track in album2_tracks_info}
+
+    common_tracks = [track for track in album1_tracks_info if track['name'] in album2_track_names]
+    unique_to_album1 = [track for track in album1_tracks_info if track['name'] not in album2_track_names]
+    unique_to_album2 = [track for track in album2_tracks_info if track['name'] not in album1_track_names]
 
     # URLs das capas dos álbuns
     album1_cover = album1_info['images'][0]['url'] if album1_info['images'] else 'https://via.placeholder.com/150'
     album2_cover = album2_info['images'][0]['url'] if album2_info['images'] else 'https://via.placeholder.com/150'
+
+    # Informações detalhadas dos álbuns
+    album1_details = {
+        'name': album1_info['name'],
+        'release_date': album1_info['release_date'],
+        'total_tracks': album1_info['total_tracks'],
+        'popularity': album1_info['popularity'],
+        'external_url': album1_info['external_urls']['spotify']
+    }
+    album2_details = {
+        'name': album2_info['name'],
+        'release_date': album2_info['release_date'],
+        'total_tracks': album2_info['total_tracks'],
+        'popularity': album2_info['popularity'],
+        'external_url': album2_info['external_urls']['spotify']
+    }
 
     return render_template('results.html', 
                           common_tracks=common_tracks, 
@@ -96,8 +115,8 @@ def compare():
                           unique_to_album2=unique_to_album2,
                           album1_cover=album1_cover,
                           album2_cover=album2_cover,
-                          album1_name=album1_info['name'],
-                          album2_name=album2_info['name'])
-
+                          album1_details=album1_details,
+                          album2_details=album2_details)
+    
 if __name__ == '__main__':
     app.run(debug=True)
